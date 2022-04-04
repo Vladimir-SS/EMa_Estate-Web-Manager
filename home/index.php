275c7e97351ca1f51921849830dad0d987387878
george.debug <george.butco@gmail.com>
2022-04-04 16:19:57 +0300

    Search Page

---
+++ home/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ro">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estence</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;1,100;1,300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../shared/styles/page.css">
+</head>
+<body>
+    <div class="page-container">
+        <?php include_once "../shared/menu.html"; ?>
+
+        <div class="content-container">
+            <div class="content">
+                
+            </div>
+        </div>
+        <!-- /.page -->
+    </div>
+    <!-- /.page-container -->
+    
+</body>
+</html>
