f70fce7b3b19844b7a6247c466d337dac753fc7d
Vladimir <stefansbarcea@yahoo.com>
2022-04-06 23:28:05 +0300

    fixed a small mistake

--- search/content.php
+++ search/content.php
@@ -3,8 +3,8 @@
         <?php include "item.php" ?>
     </div>
     <!-- /.flex-1 -->
-    <div class="flex-1 side--map">
+    <div class="flex-1 side-map">
     </div>
-    <!-- /.flex-1 side--map -->
+    <!-- /.flex-1 side-map -->
 </div>
 <!-- /.splited -->
