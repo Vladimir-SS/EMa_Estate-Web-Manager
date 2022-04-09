e7fa88a12e1c4cb39f8a7b1b792777a410785225
george.debug <george.butco@gmail.com>
2022-04-09 09:17:06 +0300

    fixed sass

--- login-register/login.php
+++ login-register/login.php
@@ -9,8 +9,9 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;1,100;1,300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../shared/styles/page.css">
+    <link rel="stylesheet" href="styles/style.css">
 </head>
 
 <body>
@@ -20,50 +21,50 @@
         <div class="content-container responsive-container">
             <div class="content">
 
-                <div class="container">
+                <div class="content__box  content__box--login">
 
                     <div class="fast-conn-buttons">
 
-                        <p class="conn-heading">Conecteaza-te</p>
-                        <button class="btn btn--gmail" type="conn-gmail">
-                            <img src="svg/gmail.svg" alt="Gmail icon">
-                            Conecteaza-te cu contul tau Google
+                        <h2 class="conn-heading">Conectează-te</h2>
+                        <button class="icon-field icon-field--gmail icon-field--social" >
+                            <?php include "svg/gmail.svg" ?>
+                            Conectează-te cu contul tau Google
                         </button>
                         <!-- /.btn btn--gmail -->
 
-                        <button class="btn btn--facebook" type="conn-facebook">
-                            <img src="svg/facebook.svg" alt="Facebook icon">
-                            Conecteaza-te cu contul tau Facebook
+                        <button class="icon-field icon-field--facebook icon-field--social" type="conn-facebook">
+                            <?php include "svg/facebook.svg" ?>
+                            Conectează-te cu contul tau Facebook
                         </button>
                         <!-- /.btn btn--gmail -->
                     </div>
                     <!-- /.fast--conn-buttons -->
-                    <form class="container__form" action="action_login.php" method="get">
+                    <form class="content__box__form" action="action_login.php" method="get">
 
                         <p>Completeaza campurile de mai jos pentru a te conecta.</p>
-                        <div class="input-box">
-                            <img src="svg/person.svg" alt="Person icon">
-                            <input class="input-box__input" type="text" placeholder="E-mail sau Telefon" name="mail_or_phone" required>
+                        <div class="icon-field input-box">
+                            <?php include "svg/person.svg" ?>
+                            <input class="input-box__input" type="text" placeholder="E-mail sau Telefon" name="mail-or-phone" required>
                         </div>
                         <!-- /.input-box -->
 
-                        <div class="input-box">
-                            <img src="svg/pass.svg" alt="Lock icon">
+                        <div class="icon-field input-box ">
+                            <?php include "svg/pass.svg" ?>
                             <input class="input-box__input input-box__input--pwd" type="password" placeholder="Introdu parola" name="psw" required>
                         </div>
                         <!-- /.input-box -->
 
                         <p> <a class="hlink" href="#">Forgot password?</a></p>
 
-                        <p>Nu ai cont? <a class="hlink" href="../login-register/register.php"> Creeaza-ti cont</a></p>
+                        <p>Nu ai cont? <a class="hlink" href="../login-register/register.php"> Creează-ți cont</a></p>
                         <p>Prin actiunea de conectare esti de acord cu
                             <a class="hlink" href="#">Termenii si Conditiile</a>, cat si
                             <a class="hlink" href="#">Politica de confidentialitate</a>
                         </p>
 
-                        <button class="btn" type="connect">
-                            <img src="svg/conn-arr.svg" alt="Connect arrow icon">
-                            Conectare
+                        <button class="icon-field submit">
+                            <?php include "svg/conn-arr.svg" ?>
+                            <p>Conectare</p>
                         </button>
                         <!-- /.btn -->
 
