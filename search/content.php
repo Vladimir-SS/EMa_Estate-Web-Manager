2c743bb812863034f9b4eaa3888803438cd56f5f
Vladimir <stefansbarcea@yahoo.com>
2022-04-16 22:34:48 +0300

    Fixes

--- search/content.php
+++ search/content.php
@@ -23,38 +23,31 @@
             </div>
 
             <div class="icon-field icon-field--filter">
-                Etaj
-                <?php include "../shared/svg/down-arrow.svg" ?>
-            </div>
-
-            <div class="icon-field icon-field--filter">
-                Preț
-                <?php include "../shared/svg/left-right-arrow.svg" ?>
-            </div>
-
-            <div class="icon-field icon-field--filter">
                 Alte opțiuni
                 <?php include "../shared/svg/right-arrow.svg" ?>
             </div>
         </div>
         <!-- /.content-filter -->
         <div>
-            <?php     
-                include "item.php";
-                include "item.php";
-                include "item.php";
-                include "item.php";
-                include "item.php";
-                include "item.php";
-                include "item.php";
-                include "item.php";
-                include "item.php";
-                include "item.php";
+            <?php
+            include "item.php";
+            include "item.php";
+            include "item.php";
+            include "item.php";
+            include "item.php";
+            include "item.php";
+            include "item.php";
+            include "item.php";
+            include "item.php";
+            include "item.php";
             ?>
         </div>
     </div>
     <!-- /.flex-1 -->
     <div class="side-map">
+        <div class="pin">
+            <p>123 000</p>
+        </div>
     </div>
     <!-- /.flex-1 side-map -->
 </div>
