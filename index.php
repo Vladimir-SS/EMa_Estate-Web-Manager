b823e6d6e490de5ba15072229871e67587327123
george.debug <george.butco@gmail.com>
2022-04-29 15:31:18 +0300

    place holder for FilterOption >

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include_once "config.php";
-
-include_once DIR_VIEWS . "View.php";
-
-echo View::render_template("page", ["title" => "Acasa", "content" => ""]);
--- views/FilterOption.php
+++ views/FilterOption.php
@@ -1,6 +1,6 @@
 <div class="icon-field icon-field--option" onclick>
     <?php
-    echo View::render_vector($vector_name);
+    echo View::render_vector("dropdown-arrow"); //$option_type
     echo $option_name;
     ?>
 </div>
