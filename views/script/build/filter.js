cf289a7aa5a2b6fa6f283c1a95ef0d205c5082dc
george.debug <george.butco@gmail.com>
2022-05-26 21:16:38 +0300

    filter

--- views/script/build/filter.js
+++ views/script/build/filter.js
@@ -1,5 +1,8 @@
 DocumentHandler.whenReady(() => {
+    let submitButton = new SubmitFilterOption();
     FilterOptionHandler
         .add(DropdownFilterOption.createWithIndex("type", ["Apartament", "Casă"], 1))
-        .add(DropdownFilterOption.createWithDefault("by", ["Proprietar", "Firmă", "Dezvoltator"], "Oricine"));
+        .add(DropdownFilterOption.createWithDefault("by", ["Proprietar", "Firmă", "Dezvoltator"], "Oricine"))
+        .add(new SliderFilterOption("price", "Preț", "Lei").set(120, 4000))
+        .add(submitButton);
 });
--- views/templates/Filter.php
+++ views/templates/Filter.php
@@ -1,2 +1,3 @@
 <div id="filter" class="filter-alignment">
+
 </div>
